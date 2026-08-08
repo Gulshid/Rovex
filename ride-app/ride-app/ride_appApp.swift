8e2d0630ffd11a08f085d5d7a7d5eee3da626b00
Gulshid Zada <gulshidzada4@gmail.com>
2026-08-08 18:29:03 +0500

    New Project started

---
+++ ride-app/ride-app/ride_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ride_appApp.swift
+//  ride-app
+//
+//  Created by Gulshid Zada on 08/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ride_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ride-app/ride-appTests/ride_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ride_appTests.swift
+//  ride-appTests
+//
+//  Created by Gulshid Zada on 08/08/2026.
+//
+
+import Testing
+@testable import ride_app
+
+struct ride_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
